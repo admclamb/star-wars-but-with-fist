f87e1f11b6d0047e5d74f8a0c13cb7952ca286ae
Anthony McLamb <90528159+admclamb@users.noreply.github.com>
2022-03-17 15:16:49 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ const Home = () => {
   const [fighter1Errors, setFighter1Errors] = useState("");
   const [fighter2, setFighter2] = useState({});
   const [fighter2Errors, setFighter2Errors] = useState("");
-  console.log(fighter1);
   // Get fighters
   useEffect(() => {
     setFighters({});
@@ -29,7 +28,6 @@ const Home = () => {
     setFighter1Errors("");
     setFighter2({});
     setFighter2Errors("");
-    setFighterReset(true);
   };
   return (
     <div className="bg-darker home">
